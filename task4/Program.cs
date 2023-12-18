62790a29944fc4715ffc6c93a7cfe3821824d2c2
Natalia <natalia.ab.ca@gmail.com>
2023-12-18 12:21:12 +0300

    Homework Урок 2. Простые Алгоритмы task4

---
+++ task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+{
+    if (N < 10)
+    {
+        Console.WriteLine(N);
+    }
+    else
+    {
+        while (N > 0)
+        {
+            int number = N % 10; 
+            N /= 10;
+            if (N > 0)
+            {
+                Console.Write(number + ",");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }        
+    }
+}
