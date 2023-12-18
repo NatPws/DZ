3d10e8f76393bad0c50befefc7611994ee7806bf
Natalia <natalia.ab.ca@gmail.com>
2023-12-18 19:16:44 +0300

    Homework Урок 4. Функции  task7 - разворот массива

---
+++ task7/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+
+int[] array = { 1, 8, 6, 12, 48, 34 };
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    int temp = array[i];
+    array[i] = array[array.Length - 1 - i];
+    array[array.Length - 1 - i] = temp;
+}
+
+Console.WriteLine(String.Join(',', array));
